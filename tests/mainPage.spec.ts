ad424716f470c4643d97ae0c4339d49190089041
George <polyakevich3427@gmail.com>
2026-05-18 20:43:44 +0300

    add describe and beforeEach

--- tests/mainPage.spec.ts
+++ tests/mainPage.spec.ts
@@ -1,8 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('тесты главной страницы', () => {
-  test('Проверка названия элементов отображения хэдера', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('https://playwright.dev/');
+  });
+  test('Проверка названия элементов отображения хэдера', async ({ page }) => {
     await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toContainText(
       'Playwright',
     );
@@ -12,7 +14,6 @@ test.describe('тесты главной страницы', () => {
     await expect(page.getByRole('link', { name: 'CLI', exact: true })).toContainText('CLI');
   });
   test('Проверка правильного значения аттрибутов href', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
     await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toHaveAttribute(
       'href',
       '/',
@@ -29,25 +30,21 @@ test.describe('тесты главной страницы', () => {
     );
   });
   test('Проверка изменения в темную тему', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
     await page.getByLabel('Switch between dark and light').click();
     await page.getByLabel('Switch between dark and light').click();
     await expect(page.locator('html')).toHaveAttribute('data-theme', 'dark');
   });
   test('Проверка отображения корректного заголовка', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
     await expect(page.getByRole('heading', { name: 'Playwright enables reliable' })).toBeVisible();
     await expect(page.getByRole('heading', { name: 'Playwright enables reliable' })).toContainText(
       'Playwright enables reliable web automation for testing, scripting, and AI agents.',
     );
   });
   test('Проверка отображения Get Started кнопки', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
     await expect.soft(page.getByRole('link', { name: 'Get started' })).toBeVisible();
     await expect.soft(page.getByRole('link', { name: 'Get started' })).toContainText('Get started');
   });
   test('Проверка отображения элементов навигации хэдера', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
     await expect(page.getByRole('link', { name: 'Docs' })).toBeVisible();
     await expect(page.getByRole('link', { name: 'MCP', exact: true })).toBeVisible();
     await expect(page.getByRole('link', { name: 'CLI', exact: true })).toBeVisible();
